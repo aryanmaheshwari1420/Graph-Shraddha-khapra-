e5a154577a3e3b85b1c61f32963a6bb3978b9678
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-06-05 23:43:59 +0530

    new file added

--- src/adjacency_list.java
+++ src/adjacency_list.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+// By default jo hai graph[] array null pada hoga toh directly hum ArrayList<> nahi create kar sakte isliye
+// empty ArrayList<> initialize krna padega jo humne kra hai line no 31 and 32 phr jaake add ki hai array list graph ke har index pe
+
 public class adjacency_list {
+    static class Edge{
+        int src;
+        int dest;
+        Edge(int src,int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of vertices: ");
-        int V = sc.nextInt(); // 4
+        int V = sc.nextInt(); // Input for the vertices
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
 
         for (int i = 0; i < graph[2].size(); i++) {
             Edge edge = graph[2].get(i);
-            System.out.println(edge.dest+" ");
+            System.out.print(edge.dest + " ");
         }
-
     }
 
     public static void createGraph(ArrayList<Edge> graph[]){
@@ -33,14 +44,5 @@ public class adjacency_list {
         graph[3].add(new Edge(3,1));
         graph[3].add(new Edge(3,2));
 
-    }
-
-    static class Edge{
-        int src;
-        int dest;
-        Edge(int src,int dest){
-            this.src = src;
-            this.dest = dest;
-        }
     }
 }
